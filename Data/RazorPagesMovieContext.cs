7487a7098d593118a8a5e4305b3f265bc95df0c0
half-winter-moon <han16064@byui.edu>
2022-05-26 17:33:27 -0600

    finished aspnet movie db page

---
+++ Data/RazorPagesMovieContext.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Data {
+    // public class RazorPagesMovieContext : DbContext
+    // {
+    //     public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+    //         : base(options)
+    //     {
+    //     }
+
+    //     public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    // }
+    public class RazorPagesMovieContext : DbContext
+    {
+#pragma warning disable CS8618
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+#pragma warning restore CS8618 
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
